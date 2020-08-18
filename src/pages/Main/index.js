5ea49bc2b00d9bb65204364c271b4e898dfc5be9
Gustavo Dias <gustavo.dias@carbonodgt.com.br>
2020-08-18 15:08:46 -0300

    Código completo

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
-
-import {View} from 'react-native';
 import Icon from 'react-native-vector-icons/MaterialIcons';
+import {Animated} from 'react-native';
+import {PanGestureHandler, State} from 'react-native-gesture-handler';
 
 import Header from '~/components/Header';
 import Tabs from '~/components/Tabs';
@@ -20,29 +20,83 @@ import {
 } from './styles.js';
 
 export default function Main() {
+  let offset = 0;
+  const translateY = new Animated.Value(0);
+
+  const animatedEvent = Animated.event(
+    [
+      {
+        nativeEvent: {
+          translationY: translateY,
+        },
+      },
+    ],
+    {useNativeDriver: true},
+  );
+
+  function onHandlerStateChange(event) {
+    if (event.nativeEvent.oldState === State.ACTIVE) {
+      let opened = false;
+      const {translationY} = event.nativeEvent;
+      offset += translationY;
+
+      if (translationY >= 100) {
+        opened = true;
+      } else {
+        translateY.setValue(offset);
+        translateY.setOffset(0);
+        offset = 0;
+      }
+      Animated.timing(translateY, {
+        toValue: opened ? 390 : 0,
+        duration: 200,
+        useNativeDriver: true,
+      }).start(() => {
+        offset = opened ? 390 : 0;
+        translateY.setOffset(offset);
+        translateY.setValue(0);
+      });
+    }
+  }
+
   return (
     <Container>
       <Header />
       <Content>
-        <Menu />
-        <Card>
-          <CardHeader>
-            <Icon name="attach-money" size={28} color="#666" />
-            <Icon name="visibility-off" size={28} color="#666" />
-          </CardHeader>
-          <CardContent>
-            <Title>Saldo disponível</Title>
-            <Description>R$ 198.611,13</Description>
-          </CardContent>
-          <CardFooter>
-            <Annotation>
-              Transferência de R$ 20,00 recebida de Paulo Fernandes hoje às
-              06:00h
-            </Annotation>
-          </CardFooter>
-        </Card>
+        <Menu translateY={translateY} />
+        <PanGestureHandler
+          onGestureEvent={animatedEvent}
+          onHandlerStateChange={onHandlerStateChange}>
+          <Card
+            style={{
+              transform: [
+                {
+                  translateY: translateY.interpolate({
+                    inputRange: [-350, 0, 390],
+                    outputRange: [-25, 0, 390],
+                    extrapolate: 'clamp',
+                  }),
+                },
+              ],
+            }}>
+            <CardHeader>
+              <Icon name="attach-money" size={28} color="#666" />
+              <Icon name="visibility-off" size={28} color="#666" />
+            </CardHeader>
+            <CardContent>
+              <Title>Saldo disponível</Title>
+              <Description>R$ 198.611,13</Description>
+            </CardContent>
+            <CardFooter>
+              <Annotation>
+                Transferência de R$ 20,00 recebida de Paulo Fernandes hoje às
+                06:00h
+              </Annotation>
+            </CardFooter>
+          </Card>
+        </PanGestureHandler>
       </Content>
-      <Tabs />
+      <Tabs translateY={translateY} />
     </Container>
   );
 }
