0402e91920146c6af89ec03626e291ddda0c0cae
Gustavo Dias <gustavo.dias@carbonodgt.com.br>
2020-08-18 13:46:15 -0300

    style: Tela inicial e card valores

--- src/pages/Main/styles.js
+++ src/pages/Main/styles.js
@@ -20,11 +20,37 @@ export const Card = styled.View`
   border-radius: 4px;
   margin: 0px 20px;
   height: 100px;
+  left: 0;
+  right: 0;
+  top: 380px;
 `;
 
-export const CardHeader = styled.View``;
-export const CardContent = styled.View``;
-export const Title = styled.Text``;
-export const Description = styled.Text``;
-export const CardFooter = styled.View``;
-export const Annotation = styled.Text``;
+export const CardHeader = styled.View`
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
+  padding: 30px;
+`;
+export const CardContent = styled.View`
+  flex: 1;
+  padding: 0 30px;
+  justify-content: center;
+`;
+export const Title = styled.Text`
+  font-size: 13px;
+  color: #999;
+`;
+export const Description = styled.Text`
+  font-size: 32px;
+  margin-top: 3px;
+  color: #333;
+`;
+export const CardFooter = styled.View`
+  padding: 30px;
+  background: #eee;
+  border-radius: 4px;
+`;
+export const Annotation = styled.Text`
+  font-size: 12px;
+  color: #333;
+`;
